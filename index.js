f6849d25b29efbe48141f22431a6b83a4961da83
Adarsh Singh <adarsh.singh@saavn.com>
2024-04-28 19:17:58 +0530

    database setup

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import Express from "express";
+import pg from "pg"
 const app = Express();
+const db = new pg.Client({
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password: process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
+  });
+  db.connect();
 app.listen(3000,()=>{
     console.log("Server running");
 })
