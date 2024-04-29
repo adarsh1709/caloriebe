bd6e37e1933734ff0df711df9d8ab5a61ce6a43b
Jatin Malav <jatin.malav@saavn.com>
2024-04-29 21:19:32 +0530

    updated .gitignore file

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import Express from "express";
 import pg from "pg"
+import dotenv from "dotenv"
+dotenv.config()
+
 const app = Express();
 const db = new pg.Client({
     user: process.env.PG_USER,
     host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
     password: process.env.PG_PASSWORD,
+    database: process.env.PG_DATABASE,
     port: process.env.PG_PORT,
   });
-  db.connect();
+  
 app.listen(3000,()=>{
     console.log("Server running");
+    db.connect().then(()=>{
+        console.log("succesfully connected to postgres")
+    }).catch((e)=>{
+        console.log(e)
+    });
 })
